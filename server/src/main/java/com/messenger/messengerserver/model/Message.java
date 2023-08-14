46487cb038281c5aefe3dad97df1a3845085d02f
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-08-14 18:05:23 +0200

    Added builder to Message model

--- server/src/main/java/com/messenger/messengerserver/model/Message.java
+++ server/src/main/java/com/messenger/messengerserver/model/Message.java
@@ -2,6 +2,7 @@ package com.messenger.messengerserver.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,7 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "message")
+@Builder
 public class Message {
 
     @Id
